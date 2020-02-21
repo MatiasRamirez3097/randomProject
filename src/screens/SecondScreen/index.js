179c64e08b0c47c0ae5503f29d2042a806d18e58
Zero3009 <matiasgr97@Outlook.com>
2020-02-21 11:31:07 -0300

    first commit

---
+++ src/screens/SecondScreen/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+class SecondScreen extends Component {
+  render() {
+    return <View />;
+  }
+}
+export default SecondScreen;
