61b2f74caa68b718c3051c6f7120aee678a6711d
Zero3009 <matiasgr97@Outlook.com>
2020-02-22 10:52:36 -0300

    installed apollo

--- App.js
+++ App.js
@@ -6,7 +6,19 @@
  * @flow
  */
 import React from 'react';
+import {ApolloClient} from 'apollo-client';
+import {ApolloProvider} from '@apollo/react-hooks';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 import RandomNavigator from './src/navigation';
+
+const client = new ApolloClient({
+  link: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+});
 export default function App() {
-  return <RandomNavigator />;
+  return (
+    <ApolloProvider client={client}>
+      <RandomNavigator />
+    </ApolloProvider>
+  );
 }
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+//import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icons from 'react-native-vector-icons/FontAwesome';
 import HomeScreen from '../screens/HomeScreen';
 import SecondScreen from '../screens/SecondScreen';
 //const Stack = createStackNavigator();
@@ -21,23 +22,27 @@ function rootStack() {
         initialRouteName="Home"
         screenOptions={({route}) => ({
           tabBarIcon: ({focused, color, size}) => {
-            //let iconName;
+            let iconName;
 
             if (route.name === 'Home') {
+              iconName = 'home';
               /*iconName = focused
                 ? 'ios-information-circle'
                 : 'ios-information-circle-outline';*/
-            } else if (route.name === 'Settings') {
+            } else if (route.name === 'Second') {
+              iconName = 'search';
               //iconName = focused ? 'ios-list-box' : 'ios-list';
             }
 
             // You can return any component that you like here!
-            //return <Ionicons name={iconName} size={size} color={color} />;
+            return <Icons name={iconName} size={size} color={color} />;
           },
         })}
         tabBarOptions={{
           activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
+          inactiveTintColor: 'white',
+          activeBackgroundColor: 'black',
+          inactiveBackgroundColor: 'black',
         }}>
         <Tabs.Screen name="Home" component={HomeScreen} />
         <Tabs.Screen name="Second" component={SecondScreen} />
